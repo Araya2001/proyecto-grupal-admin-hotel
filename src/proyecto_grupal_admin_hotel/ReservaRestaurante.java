ddc5889a8ccd88e28a630d519616605470946264
Araya2001 <araya2001@outlook.es>
2023-04-20 16:45:32 -0600

    Project

--- src/proyecto_grupal_admin_hotel/ReservaRestaurante.java
+++ src/proyecto_grupal_admin_hotel/ReservaRestaurante.java
@@ -1,21 +1,37 @@
 package proyecto_grupal_admin_hotel;
 
+import java.util.Arrays;
+
 public class ReservaRestaurante {
   private String nombreCliente;
   private int cuposReservados;
   private Restaurante restaurante;
   private TipoComida tipoComida;
-  private Mesa mesa;
+  private Mesa[] mesas;
   private boolean esConfirmada;
+  private boolean esCancelada;
   private DetalleRestaurante[] detalleRestaurante;
 
-  public ReservaRestaurante(String nombreCliente, int cuposReservados, Restaurante restaurante, TipoComida tipoComida, Mesa mesa,, boolean esConfirmada) {
+  public ReservaRestaurante(String nombreCliente, int cuposReservados, Restaurante restaurante, TipoComida tipoComida, Mesa[] mesas, boolean esConfirmada, boolean esCancelada) {
     this.nombreCliente = nombreCliente;
     this.cuposReservados = cuposReservados;
     this.restaurante = restaurante;
     this.tipoComida = tipoComida;
-    this.mesa = mesa;
+    this.mesas = mesas;
     this.esConfirmada = esConfirmada;
+    this.esCancelada = esCancelada;
+  }
+
+  public ReservaRestaurante() {
+  }
+
+  public boolean setEsCancelada() {
+    return esCancelada;
+  }
+
+  public ReservaRestaurante setEsCancelada(boolean esCancelada) {
+    this.esCancelada = esCancelada;
+    return this;
   }
 
   public DetalleRestaurante[] getDetalleRestaurante() {
@@ -25,9 +41,6 @@ public class ReservaRestaurante {
   public ReservaRestaurante setDetalleRestaurante(DetalleRestaurante[] detalleRestaurante) {
     this.detalleRestaurante = detalleRestaurante;
     return this;
-  }
-
-  public ReservaRestaurante() {
   }
 
   public boolean isEsConfirmada() {
@@ -75,12 +88,12 @@ public class ReservaRestaurante {
     return this;
   }
 
-  public Mesa getMesa() {
-    return mesa;
+  public Mesa[] getMesas() {
+    return mesas;
   }
 
-  public ReservaRestaurante setMesa(Mesa mesa) {
-    this.mesa = mesa;
+  public ReservaRestaurante setMesas(Mesa[] mesas) {
+    this.mesas = mesas;
     return this;
   }
 
@@ -138,18 +151,52 @@ public class ReservaRestaurante {
     return sb.toString();
   }
 
-
+  public String desplegarInformacion() {
+    StringBuilder sb = new StringBuilder();
+    sb.append("Información de reserva de restaurante");
+    sb.append("\n");
+    sb.append("------------------------------------------------");
+    sb.append("\n");
+    sb.append("Nombre Restaurante: ");
+    sb.append(this.restaurante.getNombre());
+    sb.append("\n");
+    sb.append("Nombre cliente: ");
+    sb.append(this.getNombreCliente());
+    sb.append("\n");
+    sb.append("Cantidad de acompañantes: ");
+    sb.append(this.getCuposReservados());
+    sb.append("\n");
+    sb.append("Tipo de comida: ");
+    sb.append(this.tipoComida.getValor());
+    sb.append("\n");
+    sb.append("Mesas: ");
+    for (int i = 0; i < this.mesas.length; i++) {
+      sb.append(this.mesas[i].getIdMesa());
+      if (i != this.mesas.length - 1) {
+        sb.append(", ");
+      }
+    }
+    sb.append("\n");
+    sb.append("Estado reserva: ");
+    sb.append(this.esConfirmada ? "CONFIRMADA" : this.esCancelada ? "CANCELADA" : "SIN CONFIRMAR");
+    sb.append("\n");
+    return sb.toString();
+  }
 
   public void cancelarReserva() {
     setEsConfirmada(false);
-    this.mesa.setDisponibilidad(true);
-    this.mesa = new Mesa()
+    setEsCancelada(true);
+    for (int i = 0; i < this.mesas.length; i++) {
+      this.mesas[i].setDisponibilidad(true);
+    }
+    this.mesas = new Mesa[1];
+    mesas[0] = new Mesa()
         .setIdMesa("MESA CANCELADA")
         .setDisponibilidad(false)
         .setAsientosDisponibles(0);
   }
 
-  public void agregarCobroAFactura(double cantidad, double monto, String comentario) {
+  public void agregarCobroFactura(double cantidad, double monto, String comentario) {
     DetalleRestaurante[] nuevoDetalle = new DetalleRestaurante[this.detalleRestaurante.length + 1];
     for (int i = 0; i < this.detalleRestaurante.length; i++) {
       nuevoDetalle[i] = this.detalleRestaurante[i];
@@ -159,5 +206,18 @@ public class ReservaRestaurante {
         .setMonto(monto)
         .setComentario(comentario);
     this.detalleRestaurante = nuevoDetalle;
+  }
+
+  @Override
+  public String toString() {
+    return "ReservaRestaurante{" +
+        "nombreCliente='" + nombreCliente + '\'' +
+        ", cuposReservados=" + cuposReservados +
+        ", restaurante=" + restaurante +
+        ", tipoComida=" + tipoComida +
+        ", mesa=" + mesas +
+        ", esConfirmada=" + esConfirmada +
+        ", detalleRestaurante=" + Arrays.toString(detalleRestaurante) +
+        '}';
   }
 }
